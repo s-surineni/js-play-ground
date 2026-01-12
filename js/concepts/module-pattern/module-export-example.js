628ec4514c7b81958a1d76669da68696c28a3d51
ssurineni <ssurineni@salesforce.com>
2026-01-12 12:35:33 +0530

    Add module export and import examples for counter functionality
    - Introduced a new module for counter management using the object module pattern.
    - Implemented methods to increment and reset the counter, demonstrating encapsulation of state.
    - Created a corresponding import example to showcase usage of the module and its methods.
    - Enhanced understanding of module patterns in JavaScript for managing shared state.

---
+++ js/concepts/module-pattern/module-export-example.js
@@ -0,0 +1,15 @@
+let counter = 0;
+
+const testModule = {
+  incrementCounter() {
+    return counter++;
+  },
+  resetCounter() {
+    console.log(`counter value prior to reset: ${counter}`);
+    counter = 0;
+  },
+};
+
+// Default export module, without name
+export default testModule;
+
---
+++ js/concepts/module-pattern/module-import-example.js
@@ -0,0 +1,11 @@
+// Usage:
+
+// Import module from path
+import testModule from './module-export-example.js';
+
+// Increment our counter
+testModule.incrementCounter();
+
+// Check the counter value and reset
+// Outputs: counter value prior to reset: 1
+testModule.resetCounter();
